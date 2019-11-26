098fb9229609c928c73dffd30aed7d63ff217b34
Nazar <nazar@openbucks.com>
2019-11-26 09:19:59 +0200

    Fix heating state parsing

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var mqtt = require('mqtt');
 var client  = mqtt.connect('mqtt://192.168.1.200', {username: 'icc', password: 'raspberry'});
 
-var d_topic = 'domoticz/in';
+var d_in_topic = 'domoticz/in';
+var d_out_topic = 'domoticz/out';
 
 client.on('connect', function () {
   client.subscribe('Inverter/#', function (err) {
@@ -42,28 +43,41 @@ var heater = {
 	"Nastya": 62
 };
 
+var thermostat = [29, 40, 39, 41];
+
 client.on('message', function (topic, message) {
-	var t = topic.split('/'), base = t[0], val, cfg;
-	if (base === "Heating" && t[2] === "state" && t[3] === "get" && heater[t[1]]) {
-		message = JSON.parse(message);
-		client.publish(d_topic, JSON.stringify({
-			idx: heater[t[1]],
-			nvalue: message.onoff
-		}));
-	} else {
-		val = t[1];
-		cfg = config[base];
-		if (cfg && cfg[val]) {
-			client.publish(d_topic, JSON.stringify({
-				idx: cfg[val],
+	if (topic === d_out_topic) {
+		message = JSON.parse.message;
+		if (thermostat.indexOf(message.idx) >=0) {
+			client.publish(d_out_topic, JSON.stringify({
+				idx: message.idx,
 				nvalue: 0,
-				svalue: message.toString()
+				svalue1: message.svalue1
 			}));
-		} else if (val === "InverterMode") {
-			client.publish(d_topic, JSON.stringify({
-				idx: modeIdx,
-				nvalue: /\(Grid\)/.test(message) ? 1 : 0,
+		}
+	} else {
+		var t = topic.split('/'), base = t[0], val, cfg;
+		if (t.length = 3 && base === "Heating" && t[2] === "state" && heater[t[1]]) {
+			message = JSON.parse(message);
+			client.publish(d_in_topic, JSON.stringify({
+				idx: heater[t[1]],
+				nvalue: message.onoff
 			}));
+		} else {
+			val = t[1];
+			cfg = config[base];
+			if (cfg && cfg[val]) {
+				client.publish(d_in_topic, JSON.stringify({
+					idx: cfg[val],
+					nvalue: 0,
+					svalue: message.toString()
+				}));
+			} else if (val === "InverterMode") {
+				client.publish(d_in_topic, JSON.stringify({
+					idx: modeIdx,
+					nvalue: /\(Grid\)/.test(message) ? 1 : 0,
+				}));
+			}
 		}
 	}
 });
